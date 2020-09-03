e794dca18cada85fef1c38d7caf85bd9e0236281
Jason Huang <jasonhlx1213@gmail.com>
2020-09-03 17:51:01 +0300

    Updates

---
+++ precache-manifest.88911d63daf12610217b632e95fb6fe9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a4e3c3269c561cc7de9577551bd4ed0",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "e9c97b84de84f0d8d725",
+    "url": "/robofriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "faf7a302bfc2863af244",
+    "url": "/robofriends/static/css/main.dc217a2a.chunk.css"
+  },
+  {
+    "revision": "e9c97b84de84f0d8d725",
+    "url": "/robofriends/static/js/2.0b5b9267.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriends/static/js/2.0b5b9267.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "faf7a302bfc2863af244",
+    "url": "/robofriends/static/js/main.84c05563.chunk.js"
+  },
+  {
+    "revision": "e7d2cbb368e0813da61d",
+    "url": "/robofriends/static/js/runtime-main.74eee7b5.js"
+  }
+]);
